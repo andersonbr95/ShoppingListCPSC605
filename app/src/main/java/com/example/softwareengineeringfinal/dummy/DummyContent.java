7e8a9cc708b7efc1b3a6ca0e780fe96200cd5365
Bryan Anderson <bryananderson@lewisu.edu>
2021-02-25 18:57:35 -0600

    Update DummyContent.java

--- app/src/main/java/com/example/softwareengineeringfinal/dummy/DummyContent.java
+++ app/src/main/java/com/example/softwareengineeringfinal/dummy/DummyContent.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
  * Helper class for providing sample content for user interfaces created by
  * Android template wizards.
  * <p>
