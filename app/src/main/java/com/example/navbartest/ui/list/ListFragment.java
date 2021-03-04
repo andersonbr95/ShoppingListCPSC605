1d76920786abcfd399f72760929197e41bf37e8b
rvilla2185 <77707454+rvilla2185@users.noreply.github.com>
2021-03-03 21:22:30 -0600

    ListFragment now sends list when pressing Add List - Will now parse list items if multiple items are added on newlines before hitting additem - buttonFind will not crash app if the button is null

--- app/src/main/java/com/example/navbartest/ui/list/ListFragment.java
+++ app/src/main/java/com/example/navbartest/ui/list/ListFragment.java
@@ -1,66 +1,70 @@
 package com.example.navbartest.ui.list;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.SearchView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.navbartest.R;
+import com.example.navbartest.ui.map.MapFragment;
 
 import java.util.ArrayList;
 
 public class ListFragment extends Fragment {
    private EditText editUserText;
    Button addItemButton;
-    ListView listFragmentListView;
-    ArrayAdapter<String> listFragmentArrayAdapter;
-    ArrayList<String> userShoppingList;
+   Button addListButton;
+   ListView listFragmentListView;
+   ArrayAdapter<String> listFragmentArrayAdapter;
+   ArrayList<String> shoppingList;
+   Bundle bundle = new Bundle();
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
-        userShoppingList = new ArrayList<String>();
+        shoppingList = new ArrayList<String>();
 
         View view = inflater.inflate(R.layout.fragment_list, container, false);
         addItemButton = view.findViewById(R.id.add_item);
+        addListButton = view.findViewById(R.id.add_list);
         editUserText = view.findViewById(R.id.edit_user_list);
 
 
         listFragmentListView = (ListView) view.findViewById(R.id.idListFragmentListView);
-        listFragmentArrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, userShoppingList);
+        listFragmentArrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, shoppingList);
         listFragmentListView.setAdapter(listFragmentArrayAdapter);
+
 
         addItemButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String userItem = editUserText.getText().toString();
-
-                userShoppingList.add(userItem);
+                String[] items = userItem.split("\n");
+                for(String item : items){
+                    shoppingList.add(item);
+                }
                 listFragmentArrayAdapter.notifyDataSetChanged();
+            }
+        });
+        /* Will add the list items and navigate to the map fragment */
+        addListButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                bundle.putStringArrayList("list", shoppingList);
+                Navigation.findNavController(view).navigate(R.id.nav_view_maps, bundle);
             }
         });
         return view;
     }
-
-    public void updateEditText(String newText){
-        editUserText.setText(newText);
-    }
-
 
 }
