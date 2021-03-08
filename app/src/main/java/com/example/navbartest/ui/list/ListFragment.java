d12f9107bf8b61cea49fdb79abfbd8612b7d24cd
Bryan Anderson <bryananderson@lewisu.edu>
2021-03-07 20:27:20 -0600

    Added Save List Function
    needs to add functionality for users to name their lists

--- app/src/main/java/com/example/navbartest/ui/list/ListFragment.java
+++ app/src/main/java/com/example/navbartest/ui/list/ListFragment.java
@@ -3,8 +3,11 @@ package com.example.navbartest.ui.list;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.Navigation;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -17,16 +20,20 @@ import android.widget.ListView;
 
 import com.example.navbartest.R;
 import com.example.navbartest.ui.map.MapFragment;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
-public class ListFragment extends Fragment {
+public class ListFragment extends Fragment implements EditListDialog.ListNameDialogListener{
    private EditText editUserText;
-   Button addItemButton, refreshListButton;
+   Button addItemButton, refreshListButton, saveListButton;
    Button addListButton;
    ListView listFragmentListView;
    ArrayAdapter<String> listFragmentArrayAdapter;
    ArrayList<String> shoppingList;
+   private DatabaseReference listDatabase;
+
    Bundle bundle = new Bundle();
 
 
@@ -40,11 +47,13 @@ public class ListFragment extends Fragment {
         addListButton = view.findViewById(R.id.add_list);
         editUserText = view.findViewById(R.id.edit_user_list);
         refreshListButton = view.findViewById(R.id.refresh);
+        saveListButton = view.findViewById(R.id.find);
 
 
         listFragmentListView = (ListView) view.findViewById(R.id.idListFragmentListView);
         listFragmentArrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, shoppingList);
         listFragmentListView.setAdapter(listFragmentArrayAdapter);
+
 
 
         addItemButton.setOnClickListener(new View.OnClickListener() {
@@ -55,6 +64,7 @@ public class ListFragment extends Fragment {
                 for(String item : items){
                     shoppingList.add(item);
                 }
+                editUserText.setText("");
                 listFragmentArrayAdapter.notifyDataSetChanged();
             }
         });
@@ -66,6 +76,16 @@ public class ListFragment extends Fragment {
                 }
             });
 
+        saveListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listDatabase = FirebaseDatabase.getInstance().getReference();
+                //openNameListDialog();
+                saveList();
+
+                listFragmentArrayAdapter.notifyDataSetChanged();
+            }
+        });
 
 
         /* Will add the list items and navigate to the map fragment */
@@ -79,5 +99,22 @@ public class ListFragment extends Fragment {
 
 
     }
+    public void openNameListDialog(){
+        EditListDialog editListName = new EditListDialog();
+        editListName.show(getParentFragmentManager(), "set list name");
+    }
 
+    @Override
+    public void applyTexts(String listName) {
+        //saveList(listName);
+    }
+
+    public void saveList(){
+
+        int i = 0;
+        while(shoppingList.size() > i){
+            listDatabase.push().child("list").child("item " + (i+1)).setValue(shoppingList.get(i));
+            i++;
+        }
+    }
 }
