23fc5f197cd07628694d2cc973ac0668e37577dc
ltoler-lewis <78184776+ltoler-lewis@users.noreply.github.com>
2021-03-11 14:07:53 -0600

    Finished "AboutUs" page and modified "Home" page

--- app/src/main/java/com/example/navbartest/ui/aboutus/AboutUsViewModel.java
+++ app/src/main/java/com/example/navbartest/ui/aboutus/AboutUsViewModel.java
@@ -11,7 +11,12 @@ public class AboutUsViewModel extends ViewModel {
 
     public AboutUsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("LorumIpsum");
+        mText.setValue("The inspiration for this app came from recently moving out of my house for the first time " +
+                "and I kept on constantly forgetting what groceries I need for my home and was a tad " +
+                "unfamiliar with the groceries stores around me. I needed a way to determine which would be the better location to get what I needed." +
+                "\n -Bryan Anderson, Founder "
+        );
+
     }
 
     public LiveData<String> getText() {
